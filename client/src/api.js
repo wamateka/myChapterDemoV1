390db034966e1ed8dc15cfb0f10c7910686e527c
bbndpx <bbndpx@umsystem.edu>
2025-10-12 16:32:50 -0500

    configuring server API for Frontend

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 // api.js
 import axios from 'axios'
-const BASE_URL = 'http://localhost:3000/api' // Your backend root
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3000/api' : 'https://mychapterdemov1.onrender.com/api' // Your backend root
 const api = axios.create({
   baseURL: BASE_URL, // Your backend root
   withCredentials: true,               // ✅ Needed for session-based auth
